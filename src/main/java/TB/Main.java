2cf4fe08ed2ecfe8e1f0b2fbac5e3ffefb79fb46
Tomasz Borowski <bortom112233@gmail.com>
2023-12-25 20:07:36 +0100

    props for streams

--- src/main/java/TB/Main.java
+++ src/main/java/TB/Main.java
@@ -1,10 +1,23 @@
 package TB;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsConfig;
 
 import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-counter");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0");
+        //if would a new customer group be added -> start listening to topic from the beginning
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+
+
     }
 }
