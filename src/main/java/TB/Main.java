287f95a16a8ee2f8a79ccffe646b14cb29e56f33
Tomasz Borowski <bortom112233@gmail.com>
2023-12-27 10:00:14 +0100

    topologia streamu

--- src/main/java/TB/Main.java
+++ src/main/java/TB/Main.java
@@ -2,8 +2,13 @@ package TB;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 public class Main {
@@ -17,7 +22,18 @@ public class Main {
         //if would a new customer group be added -> start listening to topic from the beginning
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+        streamsBuilder.<String, String>stream("sentences")
+                .flatMapValues((readOnlyKey, value) -> Arrays.asList(value.toLowerCase().split(" ")))
+                .groupBy((key, value) -> value)
+                .count(Materialized.with(Serdes.String(), Serdes.Long()))
+                .toStream()
+                .to("word-counter", Produced.with(Serdes.String(), Serdes.Long()));
 
+        KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), props);
+        kafkaStreams.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
 
     }
 }
